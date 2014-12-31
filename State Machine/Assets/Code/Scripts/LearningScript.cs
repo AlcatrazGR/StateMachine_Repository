6046a40f111ce50707aecfcb41273a5cdf09dc35
AlcatrazGR <alcatrazgr0@gmail.com>
2014-12-31 21:47:15 +0200

    HingeList

--- State Machine/Assets/Code/Scripts/LearningScript.cs
+++ State Machine/Assets/Code/Scripts/LearningScript.cs
@@ -2,20 +2,23 @@
 using System.Collections;
 
 public class LearningScript : MonoBehaviour {
-	public int number1 = 2;
-	public int number2 = 9;
+	public HingeJoint[] hingeJoints;
 
-	// Use this for initialization
+	// Use this for initialization, it is called only once at the start of the
+	//game.
 	void Start () {
+		hingeJoints = gameObject.GetComponent<HingeJoint> ();
+
+		for (int i=0; i<hingeJoints.Length(); i++) {
+			Debug.Log(hingeJoints[i]);
+		}
 	}
 	
-	// Update is called once per frame
+	// Update is called once per frame, it is called every time the frames
+	//are changed. Thats a great way to detect mouse click events or keyboard
+	//events.
 	void Update () {
-		if (Input.GetKeyUp (KeyCode.Return))
-			AddTwoNumbers ();
+
 	}
 
-	void AddTwoNumbers(){
-		Debug.Log(number1 + number2);
-	}
 }
